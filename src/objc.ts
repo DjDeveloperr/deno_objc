5c6de45853c583d921edf983d5c5fbaf5f123031
DjDeveloper <43033058+DjDeveloperr@users.noreply.github.com>
2022-03-22 06:21:57 +0000

    x

--- src/objc.ts
+++ src/objc.ts
@@ -166,6 +166,8 @@ export class ObjC {
       ...args.map((e, i) => toNative(argDefs[i + 2], e)),
     ];
 
+    console.log(obj, sel, method, argDefs, retDef, cargs);
+
     return fromNative(retDef, (fn.call as any)(...cargs));
   }
 
